1266e4413fc7de1565eb577167aa6fa41f384e30
blackprince1234 <jjj552354@gmail.com>
2023-05-25 16:48:04 -0700

    Formatted everything. Now need to insert data

--- cs35l/src/components/MyStats.js
+++ cs35l/src/components/MyStats.js
@@ -1,62 +1,85 @@
 import './MyStats.css';
 
+//Function to get cookie (might have to change)
+function getCookie(){
+    var key, value, i;
+    var cookieArray  = document.cookie.split(';');
+
+    for (i = 0; i < cookieArray.length; i++){
+        key = cookieArray[i].slice(0, cookieArray[i].indexOf("="));
+        value = cookieArray[i].slice(cookieArray[i].indexOf("=")+1);
+        if (key == 'userID'){
+            alert('userID is ' + value);
+        }
+    }
+}
+
+
 function myStats() {
+    
     return (
+        
         <div
             style={{
-                height: '100vh',
-                backgroundColor: '#02343F',
-            }}
-            className="App">
-
+                backgroundColor: '#02343F',}}
+                className="App" >
+            
+            
             <div
-                style={{ color: '#F0EDCC', fontSize: 50 }}
+                style={{ color: '#F0EDCC', fontSize: 50, padding: 0 }}
                 className="card">
                 <span className="font-link">
                     My Stats
                 </span>
             </div>
 
-
-
+            
             <div
                 style={{ color: '#F0EDCC', fontSize: 20, marginLeft: 'auto' }}
                 className="card" id="first">
-                <span className="font-link">
+
+                {/* Title */}
+                <span className="font-link" id="firstTitle">
                     Favorites
                 </span>
+    
+                <span className="font-link" id="firstData"> Fav 1 </span>
+                <span className="font-link" id="firstData"> Fav 2 </span>
+                <span className="font-link" id="firstData"> Fav 3 </span>
+                <span className="font-link" id="firstData"> Fav 4 </span>
             </div>
 
-
-
-
+        
+            {/* Second Column */}
             <div
-                style={{ color: '#F0EDCC', fontSize: 20, marginLeft: 'auto' }}
-                className="card" id="second">
-                <span className="font-link">
+                style={{ color: '#F0EDCC', fontSize: 20}}
+                className="card" id= "second" >
+                
+                <span className="font-link" id = "secondTitle">
                     Followed Artists
                 </span>
+
+                <span className="font-link" id="firstData"> Artist 1 </span>
+                <span className="font-link" id="firstData"> Artist 2 </span>
+                <span className="font-link" id="firstData"> Artist 3 </span>
+                <span className="font-link" id="firstData"> Artist 4 </span>
             </div>
 
 
-
-
+            {/* Third Column */}
             <div
-                style={{ color: '#F0EDCC', fontSize: 20, marginLeft: 'auto' }}
-                className="card" id="third">
-                <span className="font-link">
+                style={{ color: '#F0EDCC', fontSize: 20}}
+                className="card" id= "third" >
+                
+                <span className="font-link" id = "thirdTitle">
                     Listening Hours
                 </span>
+
+                <span className="font-link" id="firstData"> Artist 1 </span>
+                <span className="font-link" id="firstData"> Artist 2 </span>
+                <span className="font-link" id="firstData"> Artist 3 </span>
+                <span className="font-link" id="firstData"> Artist 4 </span>
             </div>
-
-
-
-            <div id="firstData">
-                    <span className="font-link">
-                        List of Favorites
-                    </span>
-            </div>
-
 
 
 
