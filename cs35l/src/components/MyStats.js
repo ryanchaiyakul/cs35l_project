271f52faa16f23b701e612a5972d8790d61da440
blackprince1234 <jjj552354@gmail.com>
2023-05-31 13:53:18 -0700

    Fixed the bug for get request (Bug inside the web.py file). Now able to access top_artists, recently played, and tracks through get requests

--- cs35l/src/components/MyStats.js
+++ cs35l/src/components/MyStats.js
@@ -1,4 +1,7 @@
 import './MyStats.css';
+import axios from 'axios';
+
+
 //pass user_id as a query param into the get request 
 
 let userId = "";
@@ -8,19 +11,18 @@ let userId = "";
 
 //Get request confirmed that get cookie works. 
 //Function to get the user_id for the cookie. (Find which user's statistics to show)
-function getCookie(){
+function getCookie() {
     var key, value, i;
-    var cookieArray  = document.cookie.split(';');
-    
+    var cookieArray = document.cookie.split(';');
 
-    for (i = 0; i < cookieArray.length; i++){
+
+    for (i = 0; i < cookieArray.length; i++) {
         console.log(cookieArray);
         key = cookieArray[i].slice(0, cookieArray[i].indexOf("="));
-        value = cookieArray[i].slice(cookieArray[i].indexOf("=")+1);
-        console.log("KEY: " + key);
-        if (key == 'user_id'){
-            userId = value;
+        value = cookieArray[i].slice(cookieArray[i].indexOf("=") + 1);
+        if (key == 'user_id') {
             console.log('userID is ' + value);
+            return value;
         }
     }
 }
@@ -28,33 +30,75 @@ function getCookie(){
 
 
 //Trying to put as query param into (Which redirection URL?)
-async function fetchStats(data) {
-    const response = await fetch("http://localhost:4000/get_user_stats", {
-        method: "GET", // or 'PUT'
-        body: JSON.stringify(data),
-      });
-  
-    const result = await response.json();
-    if(result.ok){
-        console.log("Succesfully retrieved the stats")
-    }
-    else{
-        console.log("Couldn't retrieve the stats");
-    }
+async function fetchStats() {
+    // const response = await fetch("http://localhost:4000/get_user_stats", {
+    //     method: "GET", // or 'PUT'
+    //     body: JSON.stringify(data),
+    //   });
+
+    //Original
+    console.log("Tyring to fetch data")
+    //  console.log("URL: " + `http://localhost:4000/_get_user_stats`, {params: {user_id: '31kdkadqj5weprsc4drjd2o6gnry'}});
+
+    //Original
+    // const response = await axios.get(`http://localhost:4000/_get_user_stats`, {params: {user_id:getCookie()}})
+
+    //Equivalent to http://localhost:4000/_get_user_stats?user_id=getCookie()
+    //request.cookies["user_id"]) not activating
+    axios.get(`http://localhost:4000/_get_user_stats`, { params: { user_id: getCookie() } })
+        .then((response) => {
+            console.log("Response: ", response.data)
+            console.log("Recently played: " + response.data.recent[0].track.name)
+        })
+        .catch(error => {
+            // Handle error
+            if (error.response) {
+
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Other error', error.message);
+            }
+        });
+
+
+
+
+
+
+    // console.log("The data retrieved: " + response.data.recent.length);
+
+    //Specify query_parameters
+    //user.get_recent_tracks(10)
+    // ttracks = await user.get_top_tracks(10)
+    // artists = await user.get_top_artists(10)
+    //console.log(response);
+
+    //const result = await response.json();
+    //console.log("Result: " + result);
+    // if(result.ok){
+    //     console.log("Succesfully retrieved the stats")
+    // }
+    // else{
+    //     console.log("Couldn't retrieve the stats");
+    // }
 }
-    
+
 
 function myStats() {
-    getCookie();
+    fetchStats()
     //URL
-    
+
     return (
         <div
             style={{
-                backgroundColor: '#02343F',}}
-                className="App" >
-            
-            
+                backgroundColor: '#02343F',
+            }}
+            className="App" >
+
+
             <div
                 style={{ color: '#F0EDCC', fontSize: 50, padding: 0 }}
                 className="card">
@@ -63,7 +107,7 @@ function myStats() {
                 </span>
             </div>
 
-            
+
             <div
                 style={{ color: '#F0EDCC', fontSize: 20, marginLeft: 'auto' }}
                 className="card" id="first">
@@ -72,20 +116,20 @@ function myStats() {
                 <span className="font-link" id="firstTitle">
                     Favorites
                 </span>
-    
+
                 <span className="font-link" id="firstData"> Fav 1 </span>
                 <span className="font-link" id="firstData"> Fav 2 </span>
                 <span className="font-link" id="firstData"> Fav 3 </span>
                 <span className="font-link" id="firstData"> Fav 4 </span>
             </div>
 
-        
+
             {/* Second Column */}
             <div
-                style={{ color: '#F0EDCC', fontSize: 20}}
-                className="card" id= "second" >
-                
-                <span className="font-link" id = "secondTitle">
+                style={{ color: '#F0EDCC', fontSize: 20 }}
+                className="card" id="second" >
+
+                <span className="font-link" id="secondTitle">
                     Followed Artists
                 </span>
 
@@ -98,10 +142,10 @@ function myStats() {
 
             {/* Third Column */}
             <div
-                style={{ color: '#F0EDCC', fontSize: 20}}
-                className="card" id= "third" >
-                
-                <span className="font-link" id = "thirdTitle">
+                style={{ color: '#F0EDCC', fontSize: 20 }}
+                className="card" id="third" >
+
+                <span className="font-link" id="thirdTitle">
                     Listening Hours
                 </span>
 
