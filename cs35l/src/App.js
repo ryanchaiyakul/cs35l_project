6d131f2869061dcf3b526176ec75650ae5c72acc
Helen Feng <53279829+hailenwashere@users.noreply.github.com>
2023-05-30 21:53:29 -0700

    update app

--- cs35l/src/App.js
+++ cs35l/src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/searchBar.js'
 import FileUpload from './components/fileUpload.js'
+import SpotifyGetPlaylists from './components/SpotifyGetPlaylists.js'
 
 function App() {
   return (
-    // <SpotifyGetPlaylists/>
-    <SpotifyLogin/>
+    <SpotifyGetPlaylists/>
   );
 }
 
