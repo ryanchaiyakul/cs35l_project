b2396f3579a77e28219c88966c5371e0d25a89e9
ryanchaiyakul <ryanchaiyakul@gmail.com>
2023-06-01 20:44:26 -0700

    wrote basic main script
    basic script that runs necessary commands or breaks if prerequistes does not exist

---
+++ main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Run build if folder does not exist
+if [ ! -d "frontend/build" ]; then
+    echo "Creating frontend build"
+    npm --prefix frontend i > /dev/null
+    npm run build --prefix frontend > /dev/null
+else
+    echo "Frontend build already exists"
+fi
+
+if [ ! -f "config.py" ]; then
+    echo "Error: config.py does not exist"
+    exit 1
+fi
+
+# Create python virtual environment if .venv does not exist
+if [ ! -d ".venv" ]; then
+    echo "Creating python virtual environment"
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+else
+    source .venv/bin/activate
+fi
+
+python web.py
