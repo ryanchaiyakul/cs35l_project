efd59459e8521a5e38a7bf4366c6e90f3499a036
Angela Tan <angelatan@Angelas-MacBook-Air.local>
2023-06-04 16:37:37 -0700

    added babel dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/searchBar.js'
 import FileUpload from './components/fileUpload.js'
-import HamburgerMenu from './pages/homescreen.js'
 import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
